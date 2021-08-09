bc4c75c12a6511a744cf3325e7e5451a4e3e7db2
camelCase1 <july1nine@gmail.com>
2021-08-08 22:09:05 -0700

    I deleted unused folders

--- app.js
+++
@@ -1,5 +0,0 @@
-const kelvin = 0
-let celsius = kelvin - 273
-let fahrenheit = celsius * (9/5) + 32
-fahrenheit = Math.floor(fahrenheit)
-console.log(`The temperature is ${fahrenheit} degrees Fahrenheit`)
