ad164263da6e65dfbfa18d075850ac757f5e3ddc
camelCase1 <july1nine@gmail.com>
2021-08-08 22:14:31 -0700

    humanYearsToDogYears

---
+++ humanYearsToDogYears.js
@@ -0,0 +1,8 @@
+let myAge = 11
+let earlyYears = 2
+earlyYears *= 10.5
+let laterYears = myAge - 2
+laterYears *= 4
+let myAgeInDogYears = laterYears + earlyYears
+let myName = 'Jaelen'.toLowerCase()
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`)
