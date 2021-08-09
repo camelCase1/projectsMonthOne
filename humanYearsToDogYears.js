62adae7d9d1e7c846bb4223d139cb592a9bbca99
camelCase1 <july1nine@gmail.com>
2021-08-08 22:26:50 -0700

    humanAgeToDogAge has entered the chat.

--- humanYearsToDogYears.js
+++ humanYearsToDogYears.js
@@ -6,3 +6,4 @@ laterYears *= 4
 let myAgeInDogYears = laterYears + earlyYears
 let myName = 'Jaelen'.toLowerCase()
 console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`)
+// big floppa
