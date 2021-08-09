99159558740ec5585b56d1babe68543c62ae3f92
camelCase1 <july1nine@gmail.com>
2021-08-08 22:07:28 -0700

    Kelvin Weather has entered the chat.

---
+++ kelvinWeather.js
@@ -0,0 +1,5 @@
+const kelvin = 0
+let celsius = kelvin - 273
+let fahrenheit = celsius * (9/5) + 32
+fahrenheit = Math.floor(fahrenheit)
+console.log(`The temperature is ${fahrenheit} degrees Fahrenheit`)
